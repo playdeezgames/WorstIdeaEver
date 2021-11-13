e89e9b29e3e8f6cd7d7c884e5391f2f625432b3a
ernestpazera <ernestpazera@gmail.com>
2021-11-13 05:26:30 -0600

    the main runner loop is established

--- Wie/Wie.Tests/RunnerRunTests.cs
+++ Wie/Wie.Tests/RunnerRunTests.cs
@@ -1,5 +1,7 @@
 using Moq;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Wie.Engine;
 using Xunit;
 
@@ -15,6 +17,24 @@ namespace Wie.Tests
             IRunner runner = new Runner();
             runner.Run(engine.Object);
             engine.Verify(x => x.IsRunning(), Times.AtLeastOnce());
+            engine.VerifyNoOtherCalls();
+        }
+        [Fact]
+        public void ShouldCallOutputAndInputMethodsAsLongAsTheEngineRuns()
+        {
+            List<bool> isRunningResults = new List<bool>() { true, true, false };
+            Mock<IEngine> engine = new();
+            engine.Setup(x => x.IsRunning()).Returns(()=> 
+            {
+                var result = isRunningResults.First();
+                isRunningResults.RemoveAt(0);
+                return result;
+            });
+            IRunner runner = new Runner();
+            runner.Run(engine.Object);
+            engine.Verify(x => x.IsRunning(), Times.Exactly(3));
+            engine.Verify(x => x.ReceiveOutput(), Times.Exactly(2));
+            engine.Verify(x => x.SendInput(It.IsAny<string>()), Times.Exactly(2));
             engine.VerifyNoOtherCalls();
         }
     }
--- Wie/Wie/Runner.fs
+++ Wie/Wie/Runner.fs
@@ -5,8 +5,7 @@ open System
 type public Runner() = 
     interface IRunner with
         member this.Run(engine:IEngine) : unit =
-            engine.IsRunning()
-            |> ignore
-        
-
-
+            while engine.IsRunning() do
+                engine.ReceiveOutput()
+                |> ignore
+                engine.SendInput("")
