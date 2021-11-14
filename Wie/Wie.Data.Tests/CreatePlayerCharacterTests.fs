0d1ee3dc82f66916712541083a02555dade8bbb8
ernestpazera <ernestpazera@gmail.com>
2021-11-14 05:57:04 -0600

    cannot make two player characters with the same name

--- Wie/Wie.Data.Tests/CreatePlayerCharacterTests.fs
+++ Wie/Wie.Data.Tests/CreatePlayerCharacterTests.fs
@@ -8,12 +8,34 @@ open Wie.Data
 let ``CreatePlayerCharacter.Should throw an exception when no world is open`` () =
     let subject : DataContext = DataContext()
     Should.Throw<NotImplementedException>(fun()-> subject.CreatePlayerCharacter(Guid.NewGuid().ToString())|>ignore) |> ignore
-
     
 [<Fact>]
 let ``CreatePlayerCharacter.Should throw an exception with an empty name`` () =
     let subject : DataContext = DataContext()
     subject.OpenWorld(Guid.NewGuid().ToString())
     Should.Throw<ArgumentException>(fun()-> subject.CreatePlayerCharacter("    ") |> ignore) |> ignore
+    subject.CloseWorld()
+
+[<Fact>]
+let ``CreatePlayerCharacter.Should create a new player character`` () =
+    let subject : DataContext = DataContext()
+    subject.OpenWorld(Guid.NewGuid().ToString())
+    let expected = subject.GetPlayerCharacters().Count + 1
+    subject.CreatePlayerCharacter(Guid.NewGuid().ToString()).ShouldNotBeNull() |> ignore
+    let actual = subject.GetPlayerCharacters().Count
+    actual.ShouldBe(expected)
+    subject.CloseWorld()
+
+
+[<Fact>]
+let ``CreatePlayerCharacter.Should not create a player character when the player character name already exists in the database`` () =
+    let subject : DataContext = DataContext()
+    subject.OpenWorld(Guid.NewGuid().ToString())
+    let name = Guid.NewGuid().ToString()
+    subject.CreatePlayerCharacter(name).ShouldNotBeNull() |> ignore
+    let expected = subject.GetPlayerCharacters().Count
+    subject.CreatePlayerCharacter(name).ShouldBeNull() |> ignore
+    let actual = subject.GetPlayerCharacters().Count
+    actual.ShouldBe(expected)
     subject.CloseWorld()
     
--- Wie/Wie.Data/PlayerCharacter.vb
+++ Wie/Wie.Data/PlayerCharacter.vb
@@ -27,6 +27,9 @@ Friend Class PlayerCharacter
         command.CommandText = "CREATE TABLE IF NOT EXISTS [PlayerCharacters]([PlayerCharacterId] INTEGER PRIMARY KEY, [PlayerCharacterName] TEXT NOT NULL UNIQUE);"
         command.ExecuteNonQuery()
     End Sub
+    Private Shared Function OfRecord(reader As SqliteDataReader) As IPlayerCharacter
+        Return New PlayerCharacter(reader.GetInt64(0), reader.GetString(1))
+    End Function
     Friend Shared Function All(connection As SqliteConnection) As List(Of IPlayerCharacter)
         AutoCreateTable(connection)
         Dim command As SqliteCommand = connection.CreateCommand
@@ -34,13 +37,33 @@ Friend Class PlayerCharacter
         Dim result As New List(Of IPlayerCharacter)
         Using reader = command.ExecuteReader
             While reader.Read()
-                result.Add(New PlayerCharacter(reader.GetInt64(0), reader.GetString(1)))
+                result.Add(OfRecord(reader))
             End While
         End Using
         Return result
     End Function
+    Private Shared Function FindByName(connection As SqliteConnection, name As String) As IPlayerCharacter
+        AutoCreateTable(connection)
+        Dim command As SqliteCommand = connection.CreateCommand
+        command.CommandText = "SELECT [PlayerCharacterId], [PlayerCharacterName] FROM [PlayerCharacters] WHERE [PlayerCharacterName]=$name;"
+        command.Parameters.AddWithValue("$name", name)
+        Using reader = command.ExecuteReader()
+            If reader.Read() Then
+                Return OfRecord(reader)
+            End If
+            Return Nothing
+        End Using
+    End Function
     Friend Shared Function Create(connection As SqliteConnection, name As String) As IPlayerCharacter
         If Not String.IsNullOrEmpty(name) Then
+            AutoCreateTable(connection)
+            If FindByName(connection, name) Is Nothing Then
+                Dim command As SqliteCommand = connection.CreateCommand
+                command.CommandText = "INSERT INTO [PlayerCharacters]([PlayerCharacterName]) VALUES($name);"
+                command.Parameters.AddWithValue("$name", name)
+                command.ExecuteNonQuery()
+                Return FindByName(connection, name)
+            End If
             Return Nothing
         End If
         Throw New ArgumentException(NameOf(name))
