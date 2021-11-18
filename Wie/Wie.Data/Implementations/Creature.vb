a9512554e4a9b14e1b2e7b2ce0d3806b5a33c602
ernestpazera <ernestpazera@gmail.com>
2021-11-18 04:55:55 -0600

    added creature dto

---
+++ Wie/Wie.Data/Implementations/Creature.vb
@@ -0,0 +1,30 @@
+﻿Friend Class Creature
+    Implements ICreature
+    Private ReadOnly _id As Long
+    Private ReadOnly _playerCharacterId As Long?
+    Private ReadOnly _locationId As Long
+
+    Public Sub New(id As Long, locationId As Long, Optional playerCharacterId As Long? = Nothing)
+        _id = id
+        _locationId = locationId
+        _playerCharacterId = playerCharacterId
+    End Sub
+
+    Public ReadOnly Property Id As Long Implements ICreature.Id
+        Get
+            Return _id
+        End Get
+    End Property
+
+    Public ReadOnly Property PlayerCharacterId As Long? Implements ICreature.PlayerCharacterId
+        Get
+            Return _playerCharacterId
+        End Get
+    End Property
+
+    Public ReadOnly Property LocationId As Long Implements ICreature.LocationId
+        Get
+            Return _locationId
+        End Get
+    End Property
+End Class
